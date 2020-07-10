9cb40379a5f0bd497b6503d8007c0b86de4d317e
rick-addison <rickaddison7634@gmail.com>
2020-07-10 16:51:25 +0900

    中身は完成（？）

--- inc/vk-filter-search/package/class-vk-filter-search.php
+++ inc/vk-filter-search/package/class-vk-filter-search.php
@@ -5,30 +5,95 @@
  * @package VK Filter Search
  */
 
-/**
- * VK Filter Search
- */
-class VK_Filter_Search {
+if ( class_exists( 'VK_Filter_Search' ) ) {
 
 	/**
-	 * Search Form
-	 *
-	 * @param array $post_types Filter Post Types.
-	 * @param array $taxonomies Filter Taxonomies.
+	 * VK Filter Search
 	 */
-	public function search_form( $post_types = array(), $taxonomies = array() ) {
+	class VK_Filter_Search {
+
+		/**
+		 * Search Form
+		 *
+		 * @param bool  $keyword    Filer by Keyword or not.
+		 * @param array $post_types Filter Post Types.
+		 * @param array $taxonomies Filter Taxonomies.
+		 */
+		public static function search_form( $keyword = true, $post_types = array(), $taxonomies = array() ) {
+			?>
+			<form method="get" action="<?php echo esc_url( site_url( '/' ) ); ?>">
+				<?php if ( true === $keyword ) : ?>
+					<label for="post_type"><?php __( 'Keyword Search', 'vk_filter_search' ); ?></label>
+					<input type="text" name="s" id="s" placeholder="<?php __( 'Input Keyword', 'vk_filter_search' ); ?>" />
+				<?php endif; ?>
+
+				<?php if ( ! empty( $post_types ) ) : ?>
+					<label for="post_type"><?php __( 'Filter by post type', 'vk_filter_search' ); ?></label>
+					<select name="post_type" id="post_type">
+					<option><?php __( 'Post Types', 'vk_filter_search' ); ?></option>
+					<?php foreach ( $post_types as $post_type ) : ?>
+						<option value="<?php echo esc_attr( $post_type ); ?>"><?php echo esc_html( get_post_type_object( $post_type->post_type )->labels->singular_name ); ?></option>
+					<?php endforeach; ?>
+					</select>
+				<?php endif; ?>
+
+				<?php
+				if ( ! empty( $taxonomies ) ) {
+					foreach ( $taxonomies as $taxonomy ) {
+
+						if ( 'category' === $taxonomy ) {
+							$taxonomy = get_taxonomy( $taxonomy );
+							echo '<label for="cat">' . __( 'Filter by', 'vk_filter_search' ) . $taxonomy->labels->singular_name . '</label>';
+							wp_dropdown_categories(
+								array(
+									'show_option_all'  => $taxonomy->labels->singular_name . __( 'list', 'vk_filter_search' ),
+									'show_option_none' => $taxonomy->labels->singular_name . __( 'none', 'vk_filter_search' ),
+									'orderby'          => 'name',
+									'hide_if_empty'    => true,
+									'selected'         => get_query_var( 'cat' ),
+									'name'             => 'cat',
+									'taxonomy'         => 'category',
+									'value_field'      => 'slug',
+								)
+							);
+						} elseif ( 'post_tag' === $taxonomy ) {
+							$taxonomy = get_taxonomy( $taxonomy );
+							echo '<label for="tag">' . __( 'Filter by', 'vk_filter_search' ) . $taxonomy->labels->singular_name . '</label>';
+							wp_dropdown_categories(
+								array(
+									'show_option_all'  => $taxonomy->labels->singular_name . __( 'list', 'vk_filter_search' ),
+									'show_option_none' => $taxonomy->labels->singular_name . __( 'none', 'vk_filter_search' ),
+									'orderby'          => 'name',
+									'hide_if_empty'    => true,
+									'selected'         => get_query_var( 'tag' ),
+									'name'             => 'tag',
+									'taxonomy'         => 'post_tag',
+									'value_field'      => 'slug',
+								)
+							);
+						} else {
+							$taxonomy = get_taxonomy( $taxonomy );
+							echo '<label for="' . $taxonomy->name . '">' . __( 'Filter by', 'vk_filter_search' ) . $taxonomy->labels->singular_name . '</label>';
+							wp_dropdown_categories(
+								array(
+									'show_option_all'  => $taxonomy->labels->singular_name . __( 'list', 'vk_filter_search' ),
+									'show_option_none' => $taxonomy->labels->singular_name . __( 'none', 'vk_filter_search' ),
+									'orderby'          => 'name',
+									'selected'         => get_query_var( $taxonomy->name ),
+									'hide_if_empty'    => true,
+									'name'             => $taxonomy->name,
+									'name'             => $taxonomy->name,
+									'value_field'      => 'slug',
+								)
+							);
+						}
+					}
+				}
+				?>
+			</form>
+			<?php
+		}
 
 	}
-
-	/**
-	 * Search Result
-	 *
-	 * @param array $post_types Filter Post Types.
-	 * @param array $taxonomies Filter Taxonomies.
-	 */
-	public function search_result( $post_types = array(), $taxonomies = array() ) {
-
-	}
-
+	new VK_Filter_Search();
 }
-new VK_Filter_Search();
