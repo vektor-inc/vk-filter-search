a565075c3f6a440b85dcb1a1daa721b6926fde91
rick-addison <rickaddison7634@gmail.com>
2020-07-22 11:03:04 +0900

    些細な修正

--- inc/vk-filter-search/vk-filter-search-config.php
+++ inc/vk-filter-search/vk-filter-search-config.php
@@ -5,7 +5,7 @@
  * @package VK Filter Search
  */
 
-if ( ! class_exists( 'VK_Filter_Search' ) ) {
+if ( ! class_exists( 'VK_Filter_Search' ) && ! class_exists( 'VK_Filter_Search_Block' ) ) {
 	require_once dirname( __FILE__ ) . '/package/class-vk-filter-search.php';
 	require_once dirname( __FILE__ ) . '/package/class-vk-filter-search-block.php';
 }
