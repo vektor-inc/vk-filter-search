0bde8161d85cabaf6a1ca09f36fb6a51845427ad
naoki <n.globe.us@gmail.com>
2020-09-29 14:34:45 +0900

    Revert "PHP の構造変更"
    This reverts commit e46ee1532dfdf201171d6d9927a1fe85981f1c4a.

--- inc/filter-search/vk-filter-search-config.php
+++ inc/filter-search/vk-filter-search-config.php
@@ -5,8 +5,7 @@
  * @package VK Filter Search
  */
 
-if ( ! class_exists( 'VK_Filter_Search' ) && ! class_exists( 'VK_Filter_Search_Block' ) && ! class_exists( 'VK_Filter_Search_Shortcode' ) ) {
+if ( ! class_exists( 'VK_Filter_Search' ) && ! class_exists( 'VK_Filter_Search_Block' ) ) {
 	require_once dirname( __FILE__ ) . '/package/class-vk-filter-search.php';
 	require_once dirname( __FILE__ ) . '/package/class-vk-filter-search-block.php';
-	require_once dirname( __FILE__ ) . '/package/class-vk-filter-search-shortcode.php';
 }
