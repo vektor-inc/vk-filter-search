0a6595558db4538cbfecfd23d487297f928f79b9
drill-lancer <rickaddison7634@gmail.com>
2020-10-01 16:45:17 +0900

    少々修正

--- inc/filter-search/vk-filter-search-config.php
+++ inc/filter-search/vk-filter-search-config.php
@@ -5,8 +5,8 @@
  * @package VK Filter Search
  */
 
-if ( ! class_exists( 'VK_Filter_Search' ) && ! class_exists( 'VK_Filter_Search_Block' ) ) {
-	// 読み込むファイルを調整
+if ( ! class_exists( 'VK_Filter_Search' ) && ! class_exists( 'VK_Filter_Search_Block' ) && ! class_exists( 'VK_Filter_Search_Shortcode' ) ) {
+	// 読み込むファイルを調整.
 	require_once dirname( __FILE__ ) . '/package/class-vk-filter-search.php';
 	include_once ABSPATH . 'wp-admin/includes/plugin.php';
 	if ( is_plugin_active( 'vk-filter-search/vk-filter-search.php' ) ) {
