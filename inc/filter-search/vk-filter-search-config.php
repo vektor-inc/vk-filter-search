19d2ffb75e1eb2edab810a4f891706543b637e48
drill-lancer <rickaddison7634@gmail.com>
2020-10-02 10:52:03 +0900

    読み込みを調整

--- inc/filter-search/vk-filter-search-config.php
+++ inc/filter-search/vk-filter-search-config.php
@@ -8,9 +8,6 @@
 if ( ! class_exists( 'VK_Filter_Search' ) && ! class_exists( 'VK_Filter_Search_Block' ) && ! class_exists( 'VK_Filter_Search_Shortcode' ) ) {
 	// 読み込むファイルを調整.
 	require_once dirname( __FILE__ ) . '/package/class-vk-filter-search.php';
-	include_once ABSPATH . 'wp-admin/includes/plugin.php';
-	if ( is_plugin_active( 'vk-filter-search/vk-filter-search.php' ) ) {
-		require_once dirname( __FILE__ ) . '/package/class-vk-filter-search-block.php';
-		require_once dirname( __FILE__ ) . '/package/class-vk-filter-search-shortcode.php';
-	}
+	require_once dirname( __FILE__ ) . '/package/class-vk-filter-search-block.php';
+	require_once dirname( __FILE__ ) . '/package/class-vk-filter-search-shortcode.php';
 }
