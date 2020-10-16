a30db85350e873164cd1ac3f47fa3ef54f2fe5da
drill-lancer <rickaddison7634@gmail.com>
2020-10-16 11:37:18 +0900

    位置変更

--- inc/filter-search/package/class-vk-filter-search-shortcode.php
+++ inc/filter-search/package/class-vk-filter-search-shortcode.php
@@ -43,11 +43,10 @@ class VK_Filter_Search_Shortcode {
 		$inner_content = shortcode_unautop( $content );
 		$content       = do_shortcode( $inner_content );
 
-		if ( false === strpos( $inner_content, 'vk_filter_search_keyword' ) ) {
-			$content .= '<input type="hidden" name="s" value="" />';
-		}
-
 		$form_after_html  = '</div>';
+		if ( false === strpos( $inner_content, 'vk_filter_search_keyword' ) ) {
+			$form_after_html .= '<input type="hidden" name="s" value="" />';
+		}
 		$form_after_html .= '<input class="btn btn-primary" type="submit" value="' . __( 'Refine search', 'vk-filter-search' ) . '" />';
 		$form_after_html .= '</form>';
 
