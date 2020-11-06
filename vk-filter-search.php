c2566e82dae3c859e372a79fd401d4a82a26194f
kurudrive <kurudrive@gmail.com>
2020-11-06 16:52:27 +0900

    [ Change version ] 0.2.1

--- vk-filter-search.php
+++ vk-filter-search.php
@@ -3,7 +3,7 @@
  * Plugin Name: VK Filter Search
  * Plugin URI: https://github.com/vektor-inc/vk-filter-search/
  * Description: This plugin for filter search.
- * Version: 0.2.0
+ * Version: 0.2.1
  * Author:  Vektor,Inc.
  * Author URI: https://vektor-inc.co.jp
  * Text Domain: vk-filter-search
