be8d76eb757f224546489d887d7779129639e258
drill-lancer <rickaddison7634@gmail.com>
2020-11-13 09:33:42 +0900

    deprecated

--- inc/filter-search/package/src/filter-search/deprecated/deprecated.js
+++ inc/filter-search/package/src/filter-search/deprecated/deprecated.js
@@ -1,7 +1,11 @@
-import { deprecated_v1 } from "./deprecated_v1";
-import { deprecated_v2 } from "./deprecated_v2";
+import { deprecated_from_0_1_0 } from "./deprecated_from_0_1_0";
+import { deprecated_from_0_1_1 } from "./deprecated_from_0_1_1";
+import { deprecated_from_0_2_3 } from "./deprecated_from_0_2_3";
+import { deprecated_from_0_3_1 } from "./deprecated_from_0_3_1";
 
 export const deprecated = [
-	deprecated_v1,
-	deprecated_v2,
+	deprecated_from_0_1_0,
+	deprecated_from_0_1_1,
+	deprecated_from_0_2_3,
+	deprecated_from_0_3_1
 ];
