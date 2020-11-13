f1387238b48bf9c937897a4bae582f58e5bbcbac
drill-lancer <rickaddison7634@gmail.com>
2020-11-13 09:22:44 +0900

    deprecated from 0.1.1

---
+++ inc/filter-search/package/src/filter-search/deprecated/deprecated_from_0_1_1.js
@@ -0,0 +1,39 @@
+const { InnerBlocks } = wp.blockEditor;
+const { __ } = wp.i18n;
+
+export const deprecated_from_0_1_0 = {
+	attributes: {
+		TargetPostType: {
+			type: 'string',
+			default: '',
+		},
+	},
+	save: ( props ) => {
+		const { attributes } = props;
+
+		const {
+			TargetPostType,
+		} = attributes;
+
+		let hiddenPostTypes;
+
+		if ( TargetPostType === '' ) {
+			hiddenPostTypes = '';
+		}
+		else {
+			hiddenPostTypes = <input type="hidden" name="vkfs_post_type[]" value={ TargetPostType } />;
+		}
+
+		return (
+				<form className={ `vk-filter-search vkfs`} method={ `get` } action={ vk_filter_search_url }>
+					<div className={ `vkfs__labels` } >
+						{ hiddenPostTypes }
+						<InnerBlocks.Content />
+					</div>
+					[no_keyword_hidden_input]
+					<input type="hidden" name="vkfs_submitted" value="true" />
+					<input className={`btn btn-primary`} type={`submit`} value={ __( 'Refine search', 'vk-filter-search' ) } />
+				</form>
+		);
+	}
+};
