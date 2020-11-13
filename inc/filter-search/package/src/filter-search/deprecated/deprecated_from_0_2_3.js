ff6a6b2d9cc1b75461ab8d817b2a29483d605098
drill-lancer <rickaddison7634@gmail.com>
2020-11-13 09:26:40 +0900

    deprecated from 0.2.3

--- inc/filter-search/package/src/filter-search/deprecated/deprecated_from_0_2_3.js
+++ inc/filter-search/package/src/filter-search/deprecated/deprecated_from_0_2_3.js
@@ -1,19 +1,26 @@
 const { InnerBlocks } = wp.blockEditor;
 const { __ } = wp.i18n;
 
-export const deprecated_from_0_1_1 = {
+export const deprecated_from_0_2_3 = {
 	attributes: {
 		TargetPostType: {
 			type: 'string',
 			default: '',
 		},
+		DisplayOnResult: {
+			type: 'boolean',
+			default: false,
+		}
 	},
 	save: ( props ) => {
 		const { attributes } = props;
 
 		const {
 			TargetPostType,
+			DisplayOnResult
 		} = attributes;
+
+		const post_id = wp.data.select("core/editor").getCurrentPostId();
 
 		let hiddenPostTypes;
 
@@ -24,6 +31,14 @@ export const deprecated_from_0_1_1 = {
 			hiddenPostTypes = <input type="hidden" name="vkfs_post_type[]" value={ TargetPostType } />;
 		}
 
+		let hiddenResult;
+		if ( DisplayOnResult ) {
+			hiddenResult = <input type="hidden" name="vkfs_form_id" value={ post_id } />;
+		} else {
+			hiddenResult = '';
+		}
+
+
 		return (
 				<form className={ `vk-filter-search vkfs`} method={ `get` } action={ vk_filter_search_url }>
 					<div className={ `vkfs__labels` } >
@@ -32,6 +47,7 @@ export const deprecated_from_0_1_1 = {
 					</div>
 					[no_keyword_hidden_input]
 					<input type="hidden" name="vkfs_submitted" value="true" />
+					{ hiddenResult }
 					<input className={`btn btn-primary`} type={`submit`} value={ __( 'Refine search', 'vk-filter-search' ) } />
 				</form>
 		);
