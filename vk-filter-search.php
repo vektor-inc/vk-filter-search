c3689fb34fc5ecbac11fa11ccc83ba69cf63b5c9
drill-lancer <rickaddison7634@gmail.com>
2020-11-19 14:19:12 +0900

    [Change Version] 0.4.17

--- vk-filter-search.php
+++ vk-filter-search.php
@@ -3,7 +3,7 @@
  * Plugin Name: VK Filter Search
  * Plugin URI: https://github.com/vektor-inc/vk-filter-search/
  * Description: This plugin for filter search.
- * Version: 0.4.16
+ * Version: 0.4.17
  * Author:  Vektor,Inc.
  * Author URI: https://vektor-inc.co.jp
  * Text Domain: vk-filter-search
