459accd22d99ea3b6d72beceaa0ece39bb8738e9
drill-lancer <rickaddison7634@gmail.com>
2021-01-20 14:59:31 +0900

    css を js で import するとおかしくなる

--- inc/filter-search/package/build/block.asset.php
+++ inc/filter-search/package/build/block.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-element', 'wp-i18n', 'wp-notices', 'wp-polyfill', 'wp-server-side-render'), 'version' => '05ca3edaa70b7731020c550599f07512');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-element', 'wp-i18n', 'wp-notices', 'wp-polyfill', 'wp-server-side-render'), 'version' => '1b3c9bb0956e07d1d78c04db1bbae415');
