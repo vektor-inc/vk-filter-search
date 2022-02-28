50db71bd40a447f052f356837fc05883788d8c0c
drill-lancer <rickaddison7634@gmail.com>
2022-02-28 11:15:33 +0900

    フィx：deploy-from-pro

--- inc/filter-search/package/src/filter-search/deprecated/0.1.1/save.js
+++ inc/filter-search/package/src/filter-search/deprecated/0.1.1/save.js
@@ -1,42 +1,42 @@
 import { InnerBlocks } from '@wordpress/block-editor';
 import { __ } from '@wordpress/i18n';
 
-export default function save(props) {
+export default function save( props ) {
 	const { attributes } = props;
 
 	const { TargetPostType } = attributes;
 
 	let hiddenPostTypes;
 
-	if (TargetPostType === '') {
+	if ( TargetPostType === '' ) {
 		hiddenPostTypes = '';
 	} else {
 		hiddenPostTypes = (
 			<input
 				type="hidden"
 				name="vkfs_post_type[]"
-				value={TargetPostType}
+				value={ TargetPostType }
 			/>
 		);
 	}
 
 	return (
 		<form
-			className={`vk-filter-search vkfs`}
-			method={`get`}
+			className={ `vk-filter-search vkfs` }
+			method={ `get` }
 			//eslint-disable-next-line camelcase,no-undef
-			action={vk_filter_search_params.home_url}
+			action={ vk_filter_search_params.home_url }
 		>
-			<div className={`vkfs__labels`}>
-				{hiddenPostTypes}
+			<div className={ `vkfs__labels` }>
+				{ hiddenPostTypes }
 				<InnerBlocks.Content />
 			</div>
 			[no_keyword_hidden_input]
 			<input type="hidden" name="vkfs_submitted" value="true" />
 			<input
-				className={`btn btn-primary`}
-				type={`submit`}
-				value={__('Refine search', 'vk-filter-search-pro')}
+				className={ `btn btn-primary` }
+				type={ `submit` }
+				value={ __( 'Refine search', 'vk-filter-search' ) }
 			/>
 		</form>
 	);
