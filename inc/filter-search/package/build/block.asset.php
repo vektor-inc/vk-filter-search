ef70dc031a7b639cf69b0a3a44a6d22b2f8cff9e
drill-lancer <rickaddison7634@gmail.com>
2022-03-02 15:45:45 +0900

    fix: build ディレクトリをコミット除外

--- inc/filter-search/package/build/block.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-element', 'wp-i18n', 'wp-notices', 'wp-server-side-render'), 'version' => '385878238208e71f66757f36027012b5');
