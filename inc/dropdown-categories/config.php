b6e31ba7cdd15397eb94d0226fb2c50920af0d05
drill-lancer <rickaddison7634@gmail.com>
2022-03-18 10:03:22 +0900

    add: dropdown-categories

---
+++ inc/dropdown-categories/config.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * VK Dropdown Categories Configulation
+ */
+
+if ( ! class_exists( 'VK_Walker_Category_Dropdown' ) ) {
+	require_once dirname( __FILE__ ) . '/package/class-vk-walker-catrgory-dropdown.php';
+}
--- vk-filter-search.php
+++ vk-filter-search.php
@@ -3,7 +3,7 @@
  * Plugin Name: VK Filter Search
  * Plugin URI: https://vk-filter-search.com/
  * Description: This plugin for filter search.
- * Version: 1.7.2
+ * Version: 1.6.1
  * Requires at least: 5.7
  * Author:  Vektor,Inc.
  * Author URI: https://vektor-inc.co.jp
@@ -28,4 +28,5 @@ global $vkfs_prefix;
 $vkfs_prefix = apply_filters( 'vkfs_prefix', 'VK ' );
 
 // Load Modules
+require_once plugin_dir_path( __FILE__ ) . 'inc/dropdown-categories/config.php';
 require_once plugin_dir_path( __FILE__ ) . 'inc/filter-search/config.php';
