909a3dfb6a18545e8b56d6504ca813238ae1e409
vektor-inc <info@vektor-inc.co.jp>
2022-06-16 00:28:29 +0000

    Update from vk-filter-search-pro

---
+++ inc/patches/patches/6.0/patch.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * VK Fiter Search Patch for WP 6.0
+ *
+ * @package VK Filter Search
+ */
+
+add_filter( 'terms_clauses', function( $clauses, $taxonomies, $args ) {
+	$get_terms_data = array(
+			'clauses' => $clauses,
+			'taxonomies' => $taxonomies,
+			'args' => $args,
+	);
+	add_filter( 'terms_pre_query', function( $terms, $class_object ) use ( $get_terms_data ) {
+		$args = $get_terms_data['args'];
+		$fields = $args['fields'];
+		$taxonomies = $get_terms_data['taxonomies'];
+
+		$cache_args = wp_array_slice_assoc( $args, array_keys( $class_object->query_var_defaults ) );
+		unset( $cache_args['pad_counts'], $cache_args['update_term_meta_cache'] );
+
+		if ( ! empty( $fields ) && 'count' !== $fields && 'all_with_object_id' !== $fields ) {
+				$cache_args['fields'] = 'all';
+		}
+
+		$key = md5( serialize( $cache_args ) . serialize( $taxonomies ) . $class_object->request );
+		$last_changed = wp_cache_get_last_changed( 'terms' );
+		$cache_key    = "get_terms:$key:$last_changed";
+
+		wp_cache_delete( $cache_key, 'terms' );
+
+		return $terms;
+	}, 10, 2 );
+	return $clauses;
+}, 10, 3 );
