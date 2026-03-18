a9a1e4f758a3c11c8171ee8de77e93287a2f1469
kurudrive <kurudrive@gmail.com>
2026-03-18 17:46:30 +0900

    Fix dist packaging excludes

--- bin/dist.sh
+++ bin/dist.sh
@@ -8,11 +8,13 @@ PLUGIN_DIR=$(cd $(dirname $(dirname $0)); pwd)
 dist_dir="${PLUGIN_DIR}/dist"
 src_dir="${dist_dir}/${PLUGIN_NAME}"
 ZIPBALL="${dist_dir}/${PLUGIN_NAME}.zip"
+DISTIGNORE="${PLUGIN_DIR}/.distignore"
 
 [[ -e "${dist_dir}" ]] || mkdir "${dist_dir}"
 [[ -e "${ZIPBALL}" ]] && rm -r "${ZIPBALL}"
+[[ -e "${src_dir}" ]] && rm -rf "${src_dir}"
 
-rsync -av "${PLUGIN_DIR}/" "${src_dir}/" --exclude="dist/" --exclude-from='.distignore'
+rsync -av "${PLUGIN_DIR}/" "${src_dir}/" --exclude="dist/" --exclude-from="${DISTIGNORE}"
 
 cd "${dist_dir}"
 
